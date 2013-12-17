8c52b9dfb3c18ef76ca2c3f655a7a6a6447d504b
viniciusss <viniciusss@me.com>
2013-12-17 07:01:21 -0200

    Removendo lixo do código.

--- Entity/IPapelEntity.php
+++ Entity/IPapelEntity.php
@@ -1,39 +1,42 @@
 <?php
 namespace Phpinheiros\Security\AuthDomain\Entity;
 
-echo __DIR__;
-exit;
 use Symfony\Component\Security\Core\Role\RoleInterface;
 use Phpinheiros\Infrastructure\ORM\IEntity;
 
 interface IPapelEntity extends RoleInterface, IEntity
 {
+
     /**
+     *
      * @return the $id
      */
     public function getId();
 
-	/**
+    /**
+     *
      * @return the $nome
      */
     public function getNome();
 
-	/**
-     * 
-     * @param string $nome
+    /**
+     *
+     * @param string $nome            
      * @return \Phpinheiros\Security\AuthDomain\Papel\IPapelEntity
      */
     public function setNome($nome);
 
-	/**
-     * @param string $role
+    /**
+     *
+     * @param string $role            
      * @return \Phpinheiros\Security\AuthDomain\Papel\IPapelEntity
      */
     public function setRole($role);
-    
+
     /**
      * Retorna o nome padrao para o papel
+     * 
      * @see \Symfony\Component\Security\Core\Role\RoleInterface::getRole()
      */
-	public function getRole();
+    public function getRole();
 }
--- Entity/IUsuarioEntity.php
+++ Entity/IUsuarioEntity.php
@@ -1,49 +1,56 @@
 <?php
 namespace Phpinheiros\Security\AuthDomain\Entity;
+
 use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 use Phpinheiros\Security\AuthDomain\Entity\IPapelEntity;
 use Phpinheiros\Infrastructure\ORM\IEntity;
 
 /**
  * Entidade de Usuario
+ * 
  * @author Vinicius de Sa <viniciusss@me.com>
  */
 interface IUsuarioEntity extends IEntity, AdvancedUserInterface, \Serializable
 {
+
     /**
      * return IPapelEntity[]
      */
     public function getFuncoes();
-    
+
     /**
      * Retorna o nome do usuario
      * return string
      */
     public function getNome();
-    
+
     /**
-     * @param string $username
+     *
+     * @param string $username            
      * @return \Phpinheiros\Security\AuthDomain\Usuario\IUsuarioEntity
      */
     public function setUsername($username);
-    
+
     /**
-     * @param string $password
+     *
+     * @param string $password            
      * @return \Phpinheiros\Security\AuthDomain\Usuario\IUsuarioEntity
      */
     public function setPassword($password);
-    
+
     /**
      * Define o nome
-     * @param string $nome
+     * 
+     * @param string $nome            
      */
     public function setNome($nome);
-    
+
     /**
-     * @param string $status
+     *
+     * @param string $status            
      * @return \Phpinheiros\Security\AuthDomain\Usuario\IUsuarioEntity
      */
     public function setStatus($status);
-    
+
     public function getStatus();
 }
